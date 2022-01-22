1cb485c61388887be47b3fbf2ef3f3fb42f67f01
M4rukku <mw784@cam.ac.uk>
2022-01-22 20:49:40 +0000

    Setup Hosting Config for Heroku

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -16,4 +16,4 @@ def about():
   
 if __name__ == "__main__":
   app.run(debug=True)
-  
+
