abd76941291ffedb7026b549214c148e3b34b213
amynichol <an588@cam.ac.uk>
2022-01-22 23:20:23 +0000

    Basic calcs through to frontend

--- app/main.py
+++ app/main.py
@@ -1,17 +1,29 @@
 from flask import Flask, render_template
+from app.sources.cryptocurrency_data_analysis.default_analysis_set import get_default_analysis_set, get_annual_energy_consumption_in_j_from_timestamp
+from app.sources.cryptocurrency_data_analysis.energy_consumption_calculations import to_scientific_notation, to_twh_from_j, j_to_co2_kg_AVG
 
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/home")
 def home():
-  return render_template("home.html", currencies = ["c1", "c2"])
+  default_analysis_set = get_default_analysis_set()
+  currencies = []
+  for an in default_analysis_set:
+    ec = to_twh_from_j(get_annual_energy_consumption_in_j_from_timestamp(an))
+    co2 = to_scientific_notation(j_to_co2_kg_AVG(ec))
+
+    entry = [an.metadata.symbol, ec, co2, "TODO", an.price, "TODO"]
+
+    currencies.append(entry)
+    # Symbol, Energy Consumed, Co2 produced, Market Cap, Energy/£,
+  return render_template("home.html", currencies = currencies)
  # return "<h1>Homepage!</h1>"
   
   
 @app.route("/about")
 def about():
-  return render_template("base.html")
+  return render_template("about.html")
   
   
 if __name__ == "__main__":
