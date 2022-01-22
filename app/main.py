3f8f21b50ef4f8b328e5366ed1f66a8778eded29
amynichol <an588@cam.ac.uk>
2022-01-22 23:30:34 +0000

    Basic calcs through to frontend

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 from app.sources.cryptocurrency_data_analysis.default_analysis_set import get_default_analysis_set, get_annual_energy_consumption_in_j_from_timestamp
 from app.sources.cryptocurrency_data_analysis.energy_consumption_calculations import to_scientific_notation, to_twh_from_j, j_to_co2_kg_AVG
-
+from app.sources.cryptocurrency_api_connections.market_cap_api import get_symbol_market_cap_map
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,11 +9,13 @@ app = Flask(__name__)
 def home():
   default_analysis_set = get_default_analysis_set()
   currencies = []
+  caps = get_symbol_market_cap_map()
   for an in default_analysis_set:
     ec = to_twh_from_j(get_annual_energy_consumption_in_j_from_timestamp(an))
     co2 = to_scientific_notation(j_to_co2_kg_AVG(ec))
-
-    entry = [an.metadata.symbol, ec, co2, "TODO", an.price, "TODO"]
+    sym = an.metadata.symbol
+    cap = caps.get(sym, "N/A")
+    entry = [sym, ec, co2, an.price,cap , "TODO"]
 
     currencies.append(entry)
     # Symbol, Energy Consumed, Co2 produced, Market Cap, Energy/£,
